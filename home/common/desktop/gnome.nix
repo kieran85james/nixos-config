af7768afa11f82771690e53a1cbbd17d19deacd1
Kieran James <kieran85james@gmail.com>
2024-05-08 19:29:20 +0100

    Update gnome shell keybindings

--- home/common/desktop/gnome.nix
+++ home/common/desktop/gnome.nix
@@ -69,7 +69,27 @@
       enable-hot-corners = false;
     };
     "org/gnome/shell/keybindings" = {
+      toggle-overview = ["<Super>"];
+      toggle-application-view = ["<Super>a"];
+      toggle-quick-settings = ["<Super>s"];
+      
       toggle-message-tray = ["<Super>v"];
+      focus-active-notification = ["<Super>n"];
+      
+      screenshot = ["<Shift>Print"];
+      screenshot-window = ["<Alt>Print"];
+      show-screenshot-ui = ["Print"];
+      show-screen-recording-ui = ["<Ctrl><Shift><Alt>R"];
+
+      switch-to-application-1 = ["<Super>1"];
+      switch-to-application-2 = ["<Super>2"];
+      switch-to-application-3 = ["<Super>3"];
+      switch-to-application-4 = ["<Super>4"];
+      switch-to-application-5 = ["<Super>5"];
+      switch-to-application-6 = ["<Super>6"];
+      switch-to-application-7 = ["<Super>7"];
+      switch-to-application-8 = ["<Super>8"];
+      switch-to-application-9 = ["<Super>9"];
     };
     "org/gnome/desktop/wm/keybindings" = {
       activate-window-menu = ["<Alt>space"];
