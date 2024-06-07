f154b0e0dfa2be3d3147dc4fa6870ac064e4b46e
Kieran James <kieran85james@gmail.com>
2024-06-07 19:09:32 +0100

    Add more file associations to xdg

--- home/common/desktop/xdg.nix
+++ home/common/desktop/xdg.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 
 let
-  browser = "google-chrome.desktop";
+  browser = ["google-chrome.desktop"];
+  archiveManager = ["org.gnome.FileRoller.desktop"];
+  imageViewer = ["org.gnome.Loupe.desktop"];
 in {
 
   xdg = {
@@ -30,35 +32,76 @@ in {
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = browser;
         "application/x-extension-htm" = browser;
         "application/x-extension-html" = browser;
         "application/x-extension-shtml" = browser;
         "application/x-extension-xht" = browser;
         "application/x-extension-xhtml" = browser;
         "application/xhtml+xml" = browser;
+        "text/html" = browser;
         "x-scheme-handler/about" = browser;
-        "x-scheme-handler/chrome" = [browser];
+        "x-scheme-handler/chrome" = browser;
         "x-scheme-handler/ftp" = browser;
         "x-scheme-handler/http" = browser;
         "x-scheme-handler/https" = browser;
         "x-scheme-handler/unknown" = browser;
+        "x-scheme-handler/spotify" = ["spotify.desktop"];
+
+        "audio/*" = ["org.gnome.Totem.dekstop"];
+        "video/*" = ["org.gnome.Totem.dekstop"];
+        "image/*" = imageViewer;
+
+        "application/json" = browser;
 
         "application/pdf" = browser;
-        "application/json" = ["gnome-text-editor.desktop"];
-        "application/toml" = "gnome-text-editor.desktop";
 
-        "text/plain" = "gnome-text-editor.desktop";
+        # Archives / compressed files
+        "application/x-7z-compressed" = archiveManager;
+        "application/x-7z-compressed-tar" = archiveManager;
+        "application/x-bzip" = archiveManager;
+        "application/x-bzip-compressed-tar" = archiveManager;
+        "application/x-compress" = archiveManager;
+        "application/x-compressed-tar" = archiveManager;
+        "application/x-cpio" = archiveManager;
+        "application/x-gzip" = archiveManager;
+        "application/x-lha" = archiveManager;
+        "application/x-lzip" = archiveManager;
+        "application/x-lzip-compressed-tar" = archiveManager;
+        "application/x-lzma" = archiveManager;
+        "application/x-lzma-compressed-tar" = archiveManager;
+        "application/x-tar" = archiveManager;
+        "application/x-tarz" = archiveManager;
+        "application/x-xar" = archiveManager;
+        "application/x-xz" = archiveManager;
+        "application/x-xz-compressed-tar" = archiveManager;
+        "application/zip" = archiveManager;
+        "application/gzip" = archiveManager;
+        "application/bzip2" = archiveManager;
+        "application/vnd.rar" = archiveManager;
 
-        "video/*" = ["org.gnome.Totem.dekstop"];
-        "video/mp4" = ["org.gnome.Totem.dekstop"];
-        "audio/*" = ["org.gnome.Totem.dekstop"];
-        "audio/mp3" = ["org.gnome.Totem.dekstop"];
-        "image/*" = ["org.gnome.loupe.desktop"];
-        "image/jpg" = ["org.gnome.loupe.desktop"];
-        "image/png" = ["org.gnome.loupe.desktop"];
-        
-        "x-scheme-handler/spotify" = ["spotify.desktop"];
+        # Images
+        "image/jpeg" = imageViewer;
+        "image/png" = imageViewer;
+        "image/gif" = imageViewer;
+        "image/webp" = imageViewer;
+        "image/tiff" = imageViewer;
+        "image/x-tga" = imageViewer;
+        "image/vnd-ms.dds" = imageViewer;
+        "image/x-dds" = imageViewer;
+        "image/bmp" = imageViewer;
+        "image/vnd.microsoft.icon" = imageViewer;
+        "image/vnd.radiance" = imageViewer;
+        "image/x-exr" = imageViewer;
+        "image/x-portable-bitmap" = imageViewer;
+        "image/x-portable-graymap" = imageViewer;
+        "image/x-portable-pixmap" = imageViewer;
+        "image/x-portable-anymap" = imageViewer;
+        "image/x-qoi" = imageViewer;
+        "image/svg+xml" = imageViewer;
+        "image/svg+xml-compressed" = imageViewer;
+        "image/avif" = imageViewer;
+        "image/heic" = imageViewer;
+        "image/jxl" = imageViewer;
       };
     };
   };
