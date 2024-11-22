5c18a332363487d88020dc0826d12ea58812a863
Kieran James <kieran85james@gmail.com>
2024-11-22 14:29:04 +0000

    Enable libvirtd on darkarmy

--- hosts/darkarmy/configuration.nix
+++ hosts/darkarmy/configuration.nix
@@ -65,6 +65,8 @@
     # gparted
   ];
 
+  virtualisation.libvirtd.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
