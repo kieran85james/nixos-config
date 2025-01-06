b34f09e90f9536b91b65e420d3ef0f067626e5b1
Kieran James <kieran85james@gmail.com>
2025-01-06 21:54:29 +0000

    Add unstable packages

--- flake.nix
+++ flake.nix
@@ -2,21 +2,26 @@
   description = "Kieran's NixOS configuration!";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
   let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    unstable = nixpkgs-unstable.legacyPackages.${system};
   in {
     nixosConfigurations = {
       # Test
       vm = lib.nixosSystem {
         inherit system;
+        specialArgs = {
+          inherit unstable;
+        };
         modules = [ ./hosts/vm/configuration.nix ];
       };
 
@@ -25,9 +30,13 @@
       # Personal
       darkarmy = lib.nixosSystem {
         inherit system;
+        specialArgs = {
+          inherit unstable;
+        };
         modules = [ ./hosts/darkarmy/configuration.nix ];
       };
     };
+    
     homeConfigurations = {
       kieran = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
--- hosts/darkarmy/configuration.nix
+++ hosts/darkarmy/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, unstable, ... }:
 
 {
   imports =
@@ -46,7 +46,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = (with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     bitwarden
     google-chrome
@@ -58,13 +58,18 @@
     rhythmbox
     steam
     mariadb
-    joplin-desktop
     ferdium
     docker
     cryptomator
     fira-mono
     # gparted
-  ];
+  ])
+
+  ++
+
+  (with unstable; [
+    joplin-desktop
+  ]);
 
   virtualisation.libvirtd.enable = true;
 
