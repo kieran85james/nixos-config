4ce6ce589fc0365d7c4829456b578958c8525455
Kieran James <kieran85james@gmail.com>
2025-11-14 23:16:50 +0000

    Make home-manager available

--- hosts/fsociety/default.nix
+++ hosts/fsociety/default.nix
@@ -4,12 +4,13 @@
     ../common/desktop
     ../common/extraServices
 
-    ../common/users/kieran.nix
-    ../common/users/gemma.nix
-
     ./configuration.nix
     # ./secrets.nix
     ./services
+
+    ../common/users/kieran.nix
+    ../common/users/gemma.nix
+    inputs.home-manager.nixosModules.home-manager
   ];
   
   extraServices.displaylink.enable = false;
