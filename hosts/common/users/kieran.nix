b294489edf61d711d4dfc0b5346b2476490233e9
Kieran James <kieran85james@gmail.com>
2025-11-17 11:56:54 +0000

    Update user description

--- hosts/common/users/kieran.nix
+++ hosts/common/users/kieran.nix
@@ -11,7 +11,7 @@ in
     # Generate password hash with command `mkpasswd mysecretpassword`
     initialHashedPassword = "$y$j9T$2dR1Z8nl.g9YjD/Acnp84.$krbE0QynKn6NiSDjA2WGuqCScYsZhH.WezG8nDMaRZ3";
     isNormalUser = true;
-    description = "${dynamicUser}";
+    description = "Kieran";
     extraGroups = [
       "wheel"
       "docker"
