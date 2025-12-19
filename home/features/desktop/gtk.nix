3259de5a506eac343176f0ff6ee206936d479257
Kieran James <kieran85james@gmail.com>
2025-12-19 16:52:32 +0000

    Correct typo in GTK bookmarks

--- home/features/desktop/gtk.nix
+++ home/features/desktop/gtk.nix
@@ -12,14 +12,14 @@ in {
   config = mkIf cfg.enable {
     gtk.enable = true;
     gtk.gtk3.bookmarks = [
-      "file:///home/${config.home.homeDirectory}/downloads Downloads"
-      "file:///home/${config.home.homeDirectory}/media/documents Documents"
-      "file:///home/${config.home.homeDirectory}/media/music Music"
-      "file:///home/${config.home.homeDirectory}/media/pictures Pictures"
-      "file:///home/${config.home.homeDirectory}/media/videos Videos"
-      "file:///home/${config.home.homeDirectory}/projects Projects"
-      "file:///home/${config.home.homeDirectory}/projects/personal Personal Projects"
-      "file:///home/${config.home.homeDirectory}/projects/jobs/app APP Wholesale"
+      "file:///${config.home.homeDirectory}/downloads Downloads"
+      "file:///${config.home.homeDirectory}/media/documents Documents"
+      "file:///${config.home.homeDirectory}/media/music Music"
+      "file:///${config.home.homeDirectory}/media/pictures Pictures"
+      "file:///${config.home.homeDirectory}/media/videos Videos"
+      "file:///${config.home.homeDirectory}/projects Projects"
+      "file:///${config.home.homeDirectory}/projects/personal Personal Projects"
+      "file:///${config.home.homeDirectory}/projects/jobs/app APP Wholesale"
       "smb://ds923-plus/ DS923-PLUS"
     ];
   };
