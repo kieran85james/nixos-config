5d4331202014fa1b97c35f192fb1dd7912e8f425
Kieran James <kieran85james@gmail.com>
2025-12-31 17:24:27 +0000

    Add home-manager configs for additional users

--- home/gemma/darkarmy.nix
+++ home/gemma/darkarmy.nix
@@ -1,3 +1,30 @@
+{ config, pkgs, ... }:
+
 {
-  # TODO: Generate darkarmy.nix for all machines as configs will be the same for user.
+  home.username = "darlene";
+  home.homeDirectory = "/home/darlene";
+  
+  imports = [
+    ../common
+
+    ../features/cli
+    ../features/desktop
+    
+    ./dotfiles
+    ./home.nix
+  ];
+
+  # Enable Home Manager modules
+  homeManagerModules.gnome.enable = true;
+
+  # Enable Home Manager features
+  features.cli.bash.enable = true;
+
+  features.desktop.xdg.enable = true;
+  features.desktop.gtk.enable = true;
+  features.desktop.fonts.enable = false;
+  features.desktop.coding.enable = false;
+  
+  # Install packages for this user only
+  home.packages = with pkgs; [];
 }
