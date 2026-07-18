b8bde2d3bb944e5ceec71187a8be4de0af2e4132
Kieran James <kieran85james@gmail.com>
2026-07-18 16:17:52 +0100

    Update formatting again

--- home/kieran/fsociety.nix
+++ home/kieran/fsociety.nix
@@ -26,10 +26,7 @@
   features.desktop.coding.enable = true;
 
   # Add extra SSH config
-  programs.ssh.extraConfig = ''
-  
-    Include /run/agenix/ssh-config-kieran
-  '';
+  programs.ssh.extraConfig = "Include /run/agenix/ssh-config-kieran";
   
   # Install packages for this user only
   home.packages = with pkgs; [];
